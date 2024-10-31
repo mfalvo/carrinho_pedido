5babd49084b5562b9524050e7b1e2958314049b0
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-10-31 14:11:18 -0300

    FIX:  correção de cardinalidade de PedidoItem

--- backend/verdurao/src/main/java/com/iconeclass/verdurao/model/PedidoItem.java
+++ backend/verdurao/src/main/java/com/iconeclass/verdurao/model/PedidoItem.java
@@ -2,11 +2,14 @@ package com.iconeclass.verdurao.model;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
 @Entity
@@ -14,9 +17,11 @@ public class PedidoItem {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name="idpedido_fk")
+	@JsonBackReference
 	private Pedido pedido;
+	
 	@OneToOne
 	@JoinColumn(name="idproduro_fk")
 	private Produto produto;
