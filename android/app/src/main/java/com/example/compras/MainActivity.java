94b452dd0abe499b6b89990d87102c0a931b1961
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-11-07 09:40:27 -0300

    NEW: projeto android studio com activities Login e Cadastramento.

---
+++ android/app/src/main/java/com/example/compras/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.compras;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        this.username = findViewById(R.id.editTextTextEmailAddress);
+        this.password = findViewById(R.id.editTextTextPassword);
+    }
+
+    // obtem dados de login
+    public void realizarLogin(View view){
+        // TODO: implementar rotina de login por API
+        Integer X = 100;
+        Integer Y = 200;
+        Integer Z = X + Y;
+    }
+
+    public void recuperarSenha(View view) {
+        // TODO: implementar rotina de recuperação de senha
+        Integer X = 100;
+        Integer Y = 200;
+        Integer Z = X + Y;
+    }
+
+    public void realizarCadastramento(View view){
+        // TODO: implementar rotina de cadastramento de novo usuário
+        Integer X = 100;
+        Integer Y = 200;
+        Integer Z = X + Y;
+    }
+}
