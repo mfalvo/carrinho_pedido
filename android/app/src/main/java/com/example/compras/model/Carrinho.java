5a5d1435d0e5176804203f78bf81dc8a759f924b
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-11-07 13:25:22 -0300

    NEW: classe Carrinho

---
+++ android/app/src/main/java/com/example/compras/model/Carrinho.java
@@ -0,0 +1,51 @@
+package com.example.compras.model;
+
+public class Carrinho {
+    private Long id;
+    private Cliente cliente;
+    private List<CarrinhoItem> carrinhoitems = new ArrayList<>();
+    private BigDecimal total = new BigDecimal(0);
+    // Getters and Setters
+
+    public Carrinho() {
+    }
+
+    public Carrinho(Long id, Cliente cliente, List<CarrinhoItem> carrinhoitems, BigDecimal total) {
+        this.id = id;
+        this.cliente = cliente;
+        this.carrinhoitems = carrinhoitems;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<CarrinhoItem> getCarrinhoitems() {
+        return carrinhoitems;
+    }
+
+    public void setCarrinhoitems(List<CarrinhoItem> carrinhoitems) {
+        this.carrinhoitems = carrinhoitems;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}
