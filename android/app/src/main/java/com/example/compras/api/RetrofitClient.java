ea4b27714ae49fe343ddfae605f39f0ad4a65683
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-11-07 13:17:19 -0300

    ENH: ajuste RetrofitClient

--- android/app/src/main/java/com/example/compras/api/RetrofitClient.java
+++ android/app/src/main/java/com/example/compras/api/RetrofitClient.java
@@ -1,2 +1,19 @@
-package com.example.compras.api;public class RetrofitClient {
+package com.example.compras.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://10.0.2.2:8080/"; // Substitua pelo seu IP e porta
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
