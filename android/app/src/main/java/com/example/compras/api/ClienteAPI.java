e8d0e06c9489d8f8fa9dd294012f2e3dce246379
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-11-08 11:27:18 -0300

    NEW: APIs de login e cadastramento de cliente

--- android/app/src/main/java/com/example/compras/api/ClienteAPI.java
+++ android/app/src/main/java/com/example/compras/api/ClienteAPI.java
@@ -4,11 +4,15 @@ import com.example.compras.model.Cliente;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ClienteAPI {
 
-        @POST("/cliente/login")
+        @GET("/clientes/login")
         Call<Cliente> login(@Body Cliente cliente);
+
+        @POST("/clientes")
+        Call<Cliente> cadastra(@Body Cliente cliente);
 
     }
