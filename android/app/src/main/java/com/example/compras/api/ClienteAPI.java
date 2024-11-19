0b15d6c4043d4c6c615255e2f0109b09b1b6a758
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-11-18 23:59:20 -0300

    ENH: documentação das APIs e inserção busca de cliente por email

--- android/app/src/main/java/com/example/compras/api/ClienteAPI.java
+++ android/app/src/main/java/com/example/compras/api/ClienteAPI.java
@@ -6,13 +6,23 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ClienteAPI {
 
+        // End Point que verifica login se usuario e senha estiverem corretos no
+        // objeto Cliente, o end point retornará o objeto Cliente completo
         @POST("/clientes/login")
         Call<Cliente> loginCliente(@Body Cliente cliente);
 
+        // End point que permite o cadastro de cliente.
         @POST("/clientes")
         Call<Cliente> cadastra(@Body Cliente cliente);
+
+        // End point que permite a obtenção do objeto cliente por email
+        @GET("/clientes/{email}")
+        Call<Cliente> getClienteByEmail(@Path("email") String email);
+
+
 
     }
