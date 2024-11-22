49cf8f942cededa4cb541a1bbd77554421f05c40
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-11-22 13:57:53 -0300

    NEW: ItemCarrinhoAdapter activity com dados quantidade e subtotal e icones +, - e seleção.

--- android/app/src/main/java/com/example/compras/adapter/ItemCarrinhoAdapter.java
+++ android/app/src/main/java/com/example/compras/adapter/ItemCarrinhoAdapter.java
@@ -16,6 +16,7 @@ import com.example.compras.R;
 import com.example.compras.model.CarrinhoItem;
 import com.example.compras.model.Produto;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,6 +56,10 @@ public class ItemCarrinhoAdapter extends RecyclerView.Adapter<ItemCarrinhoAdapte
 
         holder.nomeItemCarrinho.setText(carrinhoItem.getProduto().getNome());
         holder.precoItemCarrinho.setText(carrinhoItem.getPreco().toString());
+        holder.quantidadeItemCarrinho.setText(Integer.toString(carrinhoItem.getQuantidade()));
+        BigDecimal subtotal = new BigDecimal(carrinhoItem.getQuantidade());
+        subtotal = carrinhoItem.getPreco().multiply(new BigDecimal(carrinhoItem.getQuantidade()));
+        holder.subtotalItemCarrinho.setText(subtotal.toString());
         holder.descricaoItemCarrinho.setText(carrinhoItem.getProduto().getDescricao());
 
         if (carrinhoItem.getProduto().getImagem() != null && !carrinhoItem.getProduto().getImagem().isEmpty()) {
@@ -72,14 +77,24 @@ public class ItemCarrinhoAdapter extends RecyclerView.Adapter<ItemCarrinhoAdapte
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView nomeItemCarrinho;
+        ImageView selecaoItemCarrinho;
+        ImageView incrementaItemCarrinho;
+        TextView quantidadeItemCarrinho;
+        ImageView decrementaItemCarrinho;
         TextView precoItemCarrinho;
         TextView descricaoItemCarrinho;
         ImageView imagemItemCarrinho;
+        TextView subtotalItemCarrinho;
         public MyViewHolder(View itemView){
             super(itemView);
+            selecaoItemCarrinho = itemView.findViewById(R.id.selecaoItemCarrinho);
+            incrementaItemCarrinho = itemView.findViewById(R.id.incrementaItemCarrinho);
+            quantidadeItemCarrinho = itemView.findViewById(R.id.quantidadeItemCarrinho);
+            decrementaItemCarrinho = itemView.findViewById(R.id.decrementaItemCarrinho);
             nomeItemCarrinho = itemView.findViewById(R.id.nomeItemCarrinho);
             precoItemCarrinho = itemView.findViewById(R.id.precoItemCarrinho);
             descricaoItemCarrinho = itemView.findViewById(R.id.descricaoItemCarrinho);
+            subtotalItemCarrinho = itemView.findViewById(R.id.subtotalItemCarrinho);
             imagemItemCarrinho = itemView.findViewById(R.id.itemCarrinhoImageView);
         }
     }
